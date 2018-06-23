69aeb8b9979944d8bb5200aa4a6a26d813c01f8b
Maciej Kubas <maciekkubas@gmail.com>
2018-06-24 00:06:21 +0200

    added css

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import Navigation from './presentational/navigation.component';
 import Home from './presentational/home.component';
-import Contact from './presentational/contact.componenet';
-import NotFound from './presentational/not-found.componenet';
+import Contact from './presentational/contact.component';
+import NotFound from './presentational/not-found.component';
 
 export default (
     <Route path='/' component={Navigation}>
